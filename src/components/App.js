f1c3e4a01a877d93144e8d95fd409c99f853b6fd
duylesales <80103322+duylesales@users.noreply.github.com>
2023-11-08 18:09:20 +0700

    Restarting a Quiz

--- src/components/App.js
+++ src/components/App.js
@@ -57,6 +57,16 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.hightscore,
       };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+    // return {
+    //   ...state,
+    //   points: 0,
+    //   highscore: 0,
+    //   index: 0,
+    //   answer: null,
+    //   status: "ready",
+    // };
     default:
       throw new Error("Action unknown");
   }
@@ -115,6 +125,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
